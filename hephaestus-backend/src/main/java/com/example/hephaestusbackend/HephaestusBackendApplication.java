3557d5b625d841076952ff062b9d63012f3acd14
kingasakol <kinga.sakol@wp.pl>
2022-04-18 16:38:44 +0200

    mvn project

---
+++ hephaestus-backend/src/main/java/com/example/hephaestusbackend/HephaestusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hephaestusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HephaestusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HephaestusBackendApplication.class, args);
+	}
+
+}
---
+++ hephaestus-backend/src/test/java/com/example/hephaestusbackend/HephaestusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hephaestusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HephaestusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
