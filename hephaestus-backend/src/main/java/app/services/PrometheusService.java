4fbeb1fafed95fa28f3cf2dafdb920f14349ad5b
TheVoidek <wojtek.piechaczek@gmail.com>
2022-04-23 11:42:21 +0200

    Added prometheus url as a commandline argument

--- hephaestus-backend/src/main/java/app/services/PrometheusService.java
+++ hephaestus-backend/src/main/java/app/services/PrometheusService.java
@@ -1,6 +1,16 @@
 package app.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PrometheusService {
 
-    //todo
+    @Value("${prometheus.address}")
+    private String prometheusAddress;
+
+    public String getPrometheusAddress() {
+        return prometheusAddress;
+    }
 }
