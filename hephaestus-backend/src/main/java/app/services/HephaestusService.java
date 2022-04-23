bf740c21516b4984b5811db1760ce62bcc1b9c00
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2022-04-23 10:55:30 +0200

    Added 3rd metric for conflict tests

--- hephaestus-backend/src/main/java/app/services/HephaestusService.java
+++ hephaestus-backend/src/main/java/app/services/HephaestusService.java
@@ -17,16 +17,19 @@ public class HephaestusService {
     public ResponseEntity getMetrics() {
         //only for testing
         Map<String, String> map = new HashMap<>();
-        map.put("Label", "LabelValue");
-        map.put("AnotherLabel", "AnotherValue");
+        map.put("ThirdLabel", "ThirdValue");
+        map.put("Label2", "LabelValue2");
         ExampleMetric exampleMetric = new ExampleMetric("MetricName", map);
         Map<String, String> map2 = new HashMap<>();
-        map2.put("Label2", "LabelValue2");
         map2.put("ThirdLabel", "ThirdValue");
         ExampleMetric exampleMetric2 = new ExampleMetric("MetricName2", map2);
+        Map<String, String> map3 = new HashMap<>();
+        map3.put("Label2", "LabelValue2");
+        ExampleMetric exampleMetric3 = new ExampleMetric("MetricName2", map3);
         List<ExampleMetric> list = new ArrayList<>();
         list.add(exampleMetric);
         list.add(exampleMetric2);
+        list.add(exampleMetric3);
         ExampleMetricResponseEntity responseEntity = new ExampleMetricResponseEntity(HttpStatus.OK, list);
         return new ResponseEntity<Object>(responseEntity.toResponseMap(), responseEntity.getStatusCode());
     }
