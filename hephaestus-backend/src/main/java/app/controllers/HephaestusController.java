3443cf63ace71d1c34aea35b741dac815acac67c
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2022-05-23 00:47:44 +0200

    reading from volume on start

--- hephaestus-backend/src/main/java/app/controllers/HephaestusController.java
+++ hephaestus-backend/src/main/java/app/controllers/HephaestusController.java
@@ -2,13 +2,19 @@ package app.controllers;
 
 import app.model.Filters;
 import app.model.SelectedMetrics;
+import app.services.HephaestusService;
 import app.services.PrometheusService;
 import app.services.QueryBuilderService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import conf.Configuration;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import app.services.HephaestusService;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,12 +28,28 @@ public class HephaestusController {
     private final HephaestusService hephaestusService;
     private final QueryBuilderService queryBuilderService;
     private final PrometheusService prometheusService;
+    private final String volumePath = System.getProperty("user.dir") + "/hephaestus-backend/src/main/resources/metrics/Metrics.json";
     private List<Filters> selectedQueries;
 
     public HephaestusController(HephaestusService hephaestusService, QueryBuilderService queryBuilderService, PrometheusService prometheusService) {
         this.hephaestusService = hephaestusService;
         this.queryBuilderService = queryBuilderService;
         this.prometheusService = prometheusService;
+        // read metrics from volume
+        try {
+            this.selectedQueries = new ArrayList<>();
+            String jsonString = Files.readString(Paths.get(this.volumePath), StandardCharsets.US_ASCII);
+            JSONArray jsonArr = new JSONObject(jsonString).getJSONArray("savedMetrics");
+            ObjectMapper mapper = new ObjectMapper();
+            for (int i = 0; i < jsonArr.length(); i++) {
+                String filtersStr = jsonArr.get(i).toString();
+                Filters filters = mapper.readValue(filtersStr, Filters.class);
+                this.selectedQueries.add(filters);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Cannot read volume: " + volumePath);
+        }
     }
 
     @RequestMapping(value = "/metrics/save", method = RequestMethod.PUT)
@@ -37,9 +59,9 @@ public class HephaestusController {
     }
 
     @RequestMapping(value = "/metrics/selected", method = RequestMethod.GET)
-    public SelectedMetrics getSelected(){
+    public SelectedMetrics getSelected() {
         if (selectedQueries != null) {
-             List<String> queryResults = selectedQueries.parallelStream()
+            List<String> queryResults = selectedQueries.parallelStream()
                     .map(queryBuilderService::filtersToQuery)
                     .map(prometheusService::query)
                     .collect(Collectors.toList());
@@ -49,4 +71,10 @@ public class HephaestusController {
         }
     }
 
+    @RequestMapping(value = "/metrics/saved", method = RequestMethod.GET, produces = "application/json")
+    public List<Filters> getSaved() {
+        return this.selectedQueries;
+    }
 }
+
+
