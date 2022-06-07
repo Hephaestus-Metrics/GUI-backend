37f18d2b7d2688621e377ec52dd2057c9cc1eb1c
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2022-06-07 20:42:23 +0200

    removed hep. service and adjusted to vol manager returning null

--- hephaestus-backend/src/main/java/app/controllers/HephaestusController.java
+++ hephaestus-backend/src/main/java/app/controllers/HephaestusController.java
@@ -21,23 +21,26 @@ import java.util.stream.Collectors;
 @CrossOrigin(origins = Configuration.GUI_ORIGINS)
 public class HephaestusController {
 
-    private final HephaestusService hephaestusService;
-
+    //private final HephaestusService hephaestusService;
     private final VolumeManager volumeManager;
-
     private final QueryBuilderService queryBuilderService;
     private final PrometheusService prometheusService;
     private List<Filters> selectedQueries;
 
-    public HephaestusController(HephaestusService hephaestusService, QueryBuilderService queryBuilderService, PrometheusService prometheusService, VolumeManager volumeManager) {
-        this.hephaestusService = hephaestusService;
+    public HephaestusController(QueryBuilderService queryBuilderService, PrometheusService prometheusService, VolumeManager volumeManager) {
+        //this.hephaestusService = hephaestusService;
         this.queryBuilderService = queryBuilderService;
         this.prometheusService = prometheusService;
         this.volumeManager = volumeManager;
         // read metrics from volume
         this.selectedQueries = volumeManager.loadMetrics(false);
-        if (this.selectedQueries.size() == 0){
+        if (this.selectedQueries == null){
+            // read from config map
             this.selectedQueries = volumeManager.loadMetrics(true);
+        }
+        // all reads failed
+        if (this.selectedQueries == null){
+            this.selectedQueries = new ArrayList<>();
         }
     }
 
