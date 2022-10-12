9417f39783a92e23e9dccd0aec38d02e10f38f37
Dewkong <kryniu113@gmail.com>
2022-10-12 16:25:26 +0200

    Add tests for ExampleMetric and ResponseEntities.

--- hephaestus-backend/src/test/java/dto/ExampleMetricTest.java
+++ hephaestus-backend/src/test/java/dto/ExampleMetricTest.java
@@ -1,4 +1,80 @@
 package dto;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ExampleMetricTest {
+    private static final String NAME = "exampleName";
+    private static final String KEY = "exampleKey";
+    private static final String VALUE = "exampleValue";
+
+    @Test
+    void noAttributesSetTest() {
+        //given
+        ExampleMetric exampleMetric = new ExampleMetric();
+
+        //when
+        String metricName = exampleMetric.getName();
+        Map<String, String> metricLabels = exampleMetric.getLabels();
+
+        //then
+        assertNull(metricName);
+        assertNull(metricLabels);
+    }
+
+    @Test
+    void onlyNameSetTest() {
+        //given
+        ExampleMetric exampleMetric = new ExampleMetric();
+        exampleMetric.setName(NAME);
+
+        //when
+        String metricName = exampleMetric.getName();
+        Map<String, String> metricLabels = exampleMetric.getLabels();
+
+        //then
+        assertEquals(NAME, metricName);
+        assertNull(metricLabels);
+    }
+
+    @Test
+    void onlyLabelsSetTest() {
+        //given
+        ExampleMetric exampleMetric = new ExampleMetric();
+        exampleMetric.setLabels(new HashMap<>() {{
+            put(KEY, VALUE);
+        }});
+
+        //when
+        String metricName = exampleMetric.getName();
+        Map<String, String> metricLabels = exampleMetric.getLabels();
+
+        //then
+        assertNull(metricName);
+        assertFalse(metricLabels.isEmpty());
+        assertEquals(VALUE, metricLabels.get(KEY));
+    }
+
+    @Test
+    void everyAttributeSet() {
+        //given
+        ExampleMetric exampleMetric = new ExampleMetric();
+        exampleMetric.setName(NAME);
+        exampleMetric.setLabels(new HashMap<>() {{
+            put(KEY, VALUE);
+        }});
+
+        //when
+        String metricName = exampleMetric.getName();
+        Map<String, String> metricLabels = exampleMetric.getLabels();
+
+        //then
+        assertEquals(NAME, metricName);
+        assertFalse(metricLabels.isEmpty());
+        assertEquals(VALUE, metricLabels.get(KEY));
+    }
 }
--- hephaestus-backend/src/test/java/responses/metrics/save/SaveMetricResponseEntityTest.java
+++ hephaestus-backend/src/test/java/responses/metrics/save/SaveMetricResponseEntityTest.java
@@ -1,4 +1,34 @@
 package responses.metrics.save;
 
+import dto.ExampleMetric;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import responses.metrics.ExampleMetricResponseEntity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class SaveMetricResponseEntityTest {
+    private final static HttpStatus HTTP_STATUS = HttpStatus.OK;
+
+    private final static String MESSAGE = "message";
+
+    @Test
+    void responseMapTest() {
+        //given
+        SaveMetricResponseEntity saveMetricResponseEntity = new SaveMetricResponseEntity(HTTP_STATUS, MESSAGE);
+
+        //when
+        Map<String, Object> responseMap = saveMetricResponseEntity.toResponseMap();
+
+        //then
+        assertFalse(responseMap.isEmpty());
+        assertEquals(MESSAGE, responseMap.get("message"));
+    }
 }
