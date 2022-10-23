ed7d030f96765f79b3b92aa4ebfb2bd4227fddaf
Dewkong <kryniu113@gmail.com>
2022-10-23 14:13:41 +0200

    Add integration tests for HephaestusController.

--- hephaestus-backend/src/test/integration/app/controllers/HephaestusControllerTest.java
+++ hephaestus-backend/src/test/integration/app/controllers/HephaestusControllerTest.java
@@ -1,4 +1,77 @@
 package app.controllers;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HephaestusControllerTest {
+    @Value("${config.path}")
+    private String CONFIG_PATH;
+    @Value("${saved.path}")
+    private String SAVED_PATH;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void cleanUpBefore() {
+        cleanUp();
+    }
+
+    @AfterEach
+    void cleanUpAfter() {
+        cleanUp();
+    }
+
+    @Test
+    void getSelectedMetricsTest() throws Exception {
+        mockMvc.perform(get("/hephaestus/metrics/selected"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void saveMetricsTest() throws Exception {
+        mockMvc.perform(put("/hephaestus/metrics/save")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content("[]"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getSavedMetricsTest() throws Exception {
+        mockMvc.perform(get("/hephaestus/metrics/saved"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    private static void deleteFileFromPathIfExists(String path) {
+        File savedFile = new File(path);
+        if (savedFile.isFile()) {
+            savedFile.delete();
+        }
+    }
+
+    private void cleanUp() {
+        deleteFileFromPathIfExists(SAVED_PATH);
+        deleteFileFromPathIfExists(CONFIG_PATH);
+    }
 }
